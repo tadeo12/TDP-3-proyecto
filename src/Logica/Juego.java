2609fb4f8388e9ff25fb5c034144b04f8b2b3cba
Tadeo Villafaña <tadeoova@gmail.com>
2020-11-13 18:44:08 -0300

    commit

--- src/Logica/Juego.java
+++ src/Logica/Juego.java
@@ -1,5 +1,26 @@
 package Logica;
 
-public class Juego {
+import java.util.List;
 
+import GUI.GUI;
+
+public class Juego {
+	
+	private boolean moviendoIzquierda;
+	private boolean moviendoDerecha;
+	private boolean disparando;
+	
+	private GUI gui;
+	private List<Entidad> entidades;
+	
+	public Juego(GUI gui) {
+		moviendoIzquierda=false;
+		moviendoDerecha=false;
+		disparando=false;
+		this.gui=gui;
+		entidades= new LinkedList<Entidad>();
+	}
+	
+	
+	
 }
