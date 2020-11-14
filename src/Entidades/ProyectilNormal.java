fab89c1e75e460551a428b95c5e8ca35fae152d5
QuimeyRodi <quimeyrodi@gmail.com>
2020-11-13 21:33:31 -0300

    entidadess

--- src/Entidades/ProyectilNormal.java
+++ src/Entidades/ProyectilNormal.java
@@ -1,5 +1,12 @@
 package Entidades;
 
+import EntidadesGraficas.Entidad_grafica;
+
 public class ProyectilNormal extends ProyectilSanitario {
+
+	public ProyectilNormal(Entidad_grafica entidad) {
+		super(entidad);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/Entidades/SuperProyectil.java
+++ src/Entidades/SuperProyectil.java
@@ -1,5 +1,12 @@
 package Entidades;
 
+import EntidadesGraficas.Entidad_grafica;
+
 public class SuperProyectil extends ProyectilSanitario {
+
+	public SuperProyectil(Entidad_grafica entidad) {
+		super(entidad);
+		// TODO Auto-generated constructor stub
+	}
 
 }
