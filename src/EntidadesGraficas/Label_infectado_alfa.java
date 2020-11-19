017b336a37164e95715865e486dd928d542b31f4
ValenAle612 <valenale612@gmail.com>
2020-11-19 00:01:57 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAA :')

--- src/EntidadesGraficas/Label_infectado_alfa.java
+++ src/EntidadesGraficas/Label_infectado_alfa.java
@@ -1,5 +1,11 @@
 package EntidadesGraficas;
 
+import javax.swing.ImageIcon;
+
 public class Label_infectado_alfa extends Label_infectado{
+	
+	public Label_infectado_alfa() {
+		this.setIcon(new ImageIcon(getClass().getResource("Recursos_Graficos/infectado_alfa")));
+	}
 	
 }
--- src/EntidadesGraficas/Label_infectado_beta.java
+++ src/EntidadesGraficas/Label_infectado_beta.java
@@ -1,5 +1,11 @@
 package EntidadesGraficas;
 
+import javax.swing.ImageIcon;
+
 public class Label_infectado_beta extends Label_infectado{
 
+	public Label_infectado_beta() {
+		this.setIcon(new ImageIcon(getClass().getResource("Recursos_Graficos/infectado_beta")));
+	}
+	
 }
