2534a1d93c3e9da33c491924b395d8cc35119d93
ValenAle612 <valenale612@gmail.com>
2020-11-20 21:49:00 -0300

    gui

--- src/EntidadesGraficas/Label_infectado_alpha.java
+++ src/EntidadesGraficas/Label_infectado_alpha.java
@@ -1,5 +1,11 @@
 package EntidadesGraficas;
 
+import javax.swing.ImageIcon;
+
 public class Label_infectado_alpha extends Label_infectado{
+
+	public Label_infectado_alpha() {
+		this.setIcon(new ImageIcon(getClass().getResource("Recursos_Graficos/infectado_alfa")));
+	}
 	
 }
