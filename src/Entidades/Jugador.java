6a23ee6a4987183ff76b81c3d8d34101a693c752
Tadeo Villafaña <tadeoova@gmail.com>
2020-12-07 17:34:48 -0300

    no se

--- src/Entidades/Jugador.java
+++ src/Entidades/Jugador.java
@@ -1,7 +1,5 @@
 package Entidades;
 
-import java.util.LinkedList;
-
 import EntidadesGraficas.Label_jugador;
 import EstadosArma.ConArmaNormal;
 import EstadosArma.EstadoArma;
@@ -16,7 +14,6 @@ public class Jugador extends Entidad {
 	protected int carga_viral;
 	protected int tiros;
 	
-	protected LinkedList<Object> listaEstados;
 
 	public Jugador() {
 		super(new Label_jugador());
@@ -24,7 +21,6 @@ public class Jugador extends Entidad {
 		estado_arma = new ConArmaNormal(this);
 		estado_jugador = new EstadoInicial(this);
 		carga_viral = 0;
-		
 		tiros=0;
 	}
 
@@ -83,7 +79,6 @@ public class Jugador extends Entidad {
 
 	public void setEstadoJugador(EstadoJugador estado_jugador) {
 		this.estado_jugador = estado_jugador;
-		//System.out.println("cambiando estado: " + estado_jugador);
 	}
 
 	public void setEstadoArma(EstadoArma estado_arma) {
@@ -100,7 +95,6 @@ public class Jugador extends Entidad {
 
 	@Override
 	public int getVelocidad() {
-		// System.out.println(estado_jugador.getVelocidad());
 		return estado_jugador.getVelocidad();
 	}
 }
