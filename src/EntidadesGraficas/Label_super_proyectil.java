585adcd20028ac95d6965fc293afea78e9b2fd26
Tadeo Villafaña <tadeoova@gmail.com>
2020-12-08 16:14:22 -0300

    ahora si guarde

--- src/EntidadesGraficas/Label_super_proyectil.java
+++ src/EntidadesGraficas/Label_super_proyectil.java
@@ -5,13 +5,9 @@ import java.util.Random;
 
 import javax.swing.ImageIcon;
 
-<<<<<<< HEAD
 public class Label_super_proyectil extends Label_proyectil_sanitario{
 	
-=======
-public class Label_super_proyectil extends Label_proyectil_sanitario {
 
->>>>>>> f6d678e5614141ef496e3b0681f27469a389b3f9
 	public Label_super_proyectil(Point p) {
 		super(p);
 		this.setSize(50, 100);
