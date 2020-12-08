f2a5bc1d4633c3bdf51eab54283ddc8b3e38eb9d
QuimeyRodi <quimeyrodi@gmail.com>
2020-12-08 16:14:00 -0300

    proy sanitario

--- src/EntidadesGraficas/Label_super_arma.java
+++ src/EntidadesGraficas/Label_super_arma.java
@@ -4,11 +4,12 @@ import java.awt.Point;
 
 import javax.swing.ImageIcon;
 
-public class Label_super_arma extends Label_premio_temporal {
+public class Label_super_arma extends Label_premio_temporal
+{
 
 	public Label_super_arma(Point p) {
 		super(p);
-		ImageIcon imagen = new ImageIcon(getClass().getResource("/RecursosGraficos_Premios/superArma.gif"));
+		ImageIcon imagen = new ImageIcon(getClass().getResource("/RecursosGraficos_Premios/superArma.png"));
 		this.setIcon(imagen);
 		reDimensionar(this, imagen);
 	}
