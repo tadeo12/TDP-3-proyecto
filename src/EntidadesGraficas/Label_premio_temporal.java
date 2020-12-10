d8dec5aca5d4f42039cf3dd2987eab7b7a124e54
QuimeyRodi <quimeyrodi@gmail.com>
2020-12-10 12:27:41 -0300

    comments3

--- src/EntidadesGraficas/Label_premio_temporal.java
+++ src/EntidadesGraficas/Label_premio_temporal.java
@@ -2,7 +2,7 @@ package EntidadesGraficas;
 
 import java.awt.Point;
 
-public class Label_premio_temporal extends Label_premio {
+public abstract class Label_premio_temporal extends Label_premio {
 
 	public Label_premio_temporal(Point p) {
 		super(p);
