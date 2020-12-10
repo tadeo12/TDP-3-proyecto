2e92ef6f544eafb8918fb3e44c892ac718fe1d9a
Tadeo Villafaña <tadeoova@gmail.com>
2020-12-10 10:18:18 -0300

    ya

--- src/EntidadesGraficas/Label_jugador.java
+++ src/EntidadesGraficas/Label_jugador.java
@@ -3,7 +3,9 @@ package EntidadesGraficas;
 import javax.swing.ImageIcon;
 
 public class Label_jugador extends Entidad_grafica {
-
+	
+	private final int mejorasSinAfectarJugador=1;//bajoCero
+	
 	private String[] rutasImagen = { "/RecursosGraficos_Jugador/uy-kieto.png",
 			"/RecursosGraficos_Jugador/uy-kieto inmunidad.png", "/RecursosGraficos_Jugador/uy-kieto superArma.png",
 			"/RecursosGraficos_Jugador/uy-kieto velocidad.png" };
@@ -18,14 +20,22 @@ public class Label_jugador extends Entidad_grafica {
 		this.setVisible(true);
 	}
 
+	/**
+	 * metodo utilizado para cambiar la imagen del jugador cuando posee alguna
+	 * mejora.
+	 * 
+	 * @param mejoras Arreglo de booleans que representan las mejoras que recibió el
+	 *                jugador. No se tiene en cuenta los primeros elementos del
+	 *                arreglo ya que seran los que no afectan directamente al jugador
+	 */
 	public void setPowerUp(boolean[] mejoras) {
 		boolean encontre = false;
-		for (int i = 1; i < mejoras.length && !encontre; i++)
+		for (int i = this.mejorasSinAfectarJugador; i < mejoras.length && !encontre; i++)
 			if (mejoras[i]) {
 				super.reDimensionar(this, new ImageIcon(Label_jugador.class.getResource(rutasImagen[i])));
-				encontre=true;
+				encontre = true;
 			}
-		if(!encontre)
+		if (!encontre)
 			super.reDimensionar(this, new ImageIcon(getClass().getResource(rutasImagen[0])));
 	}
 }
